f798593585e235632e6f570a76da673a2bbf91ba
bgy <bgy@bgy.com>
2018-12-12 19:11:07 +0800

    add package esp_control

---
+++ esp_control/main/user_main.c
@@ -0,0 +1,59 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+   */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "driver/uart.h"
+
+#include "esp_log.h"
+#include "esp_system.h" 
+
+#define BUF_SIZE (1024)
+
+static void control_task()
+{
+    // Configure parameters of an UART driver,
+    // communication pins and install the driver
+    uart_config_t uart_config = {
+        .baud_rate = 9600,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+    };
+    uart_param_config(UART_NUM_0, &uart_config);
+    uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL);
+
+    // Configure a temporary buffer for the incoming data
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+
+    while (1) {
+        // Read data from the UART
+        int len = uart_read_bytes(UART_NUM_0, data, BUF_SIZE, 20 / portTICK_RATE_MS);
+        // Write data back to the UART
+        uart_write_bytes(UART_NUM_0, (const char *) data, len);
+    }
+}
+
+/******************************************************************************
+ * FunctionName : app_main
+ * Description  : entry of user application, init user function here
+ * Parameters   : none
+ * Returns      : none
+ *******************************************************************************/
+void app_main(void)
+{
+	printf("SDK version:%s\n", esp_get_idf_version());
+
+	xTaskCreate(control_task, "control_task", 4096, NULL, 5, NULL);
+}
