dc50fa953c76f1fdc03176e4706d137b751ea0cc
Jokebin <920923957@qq.com>
2019-06-16 19:27:29 +0800

    update vimrc

---
+++ vimconfig/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt-get install vim cscope
+cp vimrc.conf ~/.vimrc
+mkdir -p ~/.vim/bundle
+mkdir -p ~/.vim/cscope_db
+
+cp plugin ~/.vim -rf
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+git clone https://github.com/tomasr/molokai.git
+mv molokai/colors ~/.vim
+rm molokai -rf
